698416939545b408d38904991d13115760a68957
Dmitriy <48031182+dimaarion@users.noreply.github.com>
2023-02-12 16:01:54 +0200

    Draft/kind ride (#1)
    * Update 3 files
    
    * Update index.css and index.html
    
    * Update index.html
    
    * Update index.css and index.html
    
    * Update 3 files
    
    * Update index.html and index.js
    
    * Update index.css and index.html

---
+++ index.js
@@ -0,0 +1,24 @@
+document.querySelectorAll(".dropdown-hov").forEach((e, i) => {
+  e.addEventListener("mouseover", () => {
+    document.querySelectorAll(".dropdown-menu").forEach((el, j) => {
+      if (i == j) {
+        el.classList.add("dropdown-hov-menu");
+      } else {
+        el.classList.remove("dropdown-hov-menu");
+      }
+      //
+    });
+  });
+});
+
+document.querySelector("body").addEventListener("click", () => {
+  document.querySelectorAll(".dropdown-menu").forEach((el) => {
+    el.classList.remove("dropdown-hov-menu");
+  });
+});
+
+window.addEventListener("scroll", () => {
+  document.querySelectorAll(".dropdown-menu").forEach((el) => {
+    el.classList.remove("dropdown-hov-menu");
+  });
+});
